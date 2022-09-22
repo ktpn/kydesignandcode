be01b7028507968c56d84311865abb3d0089dbb6
ktpn <kypetern@gmail.com>
2022-09-22 14:11:32 -0500

    Added horizontal scrolleffect

--- script.js
+++ script.js
@@ -12,3 +12,64 @@ function reveal() {
     }
   }
   window.addEventListener("scroll", reveal);
+
+  
+
+  (function(){
+    init();
+
+    function init(){
+        setStickyContainersSize();
+        bindEvents();
+    }
+
+    function bindEvents(){
+      trackScroll();
+    }
+
+    function setStickyContainersSize(){
+        document.querySelectorAll('.sticky-container').forEach(function(container){
+            const stikyContainerHeight = container.querySelector('main').scrollWidth;
+            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+        });
+    }
+
+    function isElementInViewport (el) {
+        const rect = el.getBoundingClientRect();
+        return rect.top <= 0 && rect.bottom >= document.documentElement.clientHeight;
+    }
+
+    function trackScroll(){
+        const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
+            const isVisible = isElementInViewport(container);
+            if(!isVisible){
+              var position;
+              var containerWidth = container.offsetTop + container.offsetWidth;
+              if(containerWidth < window.pageYOffset){
+                container.querySelector('main').scrollLeft = containerWidth;
+              }else if(container.offsetTop < window.pageYOffset){
+  
+                container.querySelector('main').scrollLeft = 0;
+              }
+            }
+            return isVisible;
+        })[0];
+
+        if(!containerInViewPort){
+            requestAnimationFrame(trackScroll);
+            return;
+        }
+
+        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+        if(g_canScrollHorizontally){
+            var pxToScroll = window.pageYOffset - containerInViewPort.offsetTop;
+            containerInViewPort.querySelector('main').scrollLeft = pxToScroll;
+        }
+      
+      requestAnimationFrame(trackScroll);
+    }
+})();
+
